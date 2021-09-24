f17ddaf66313dbede6adcc0b3f0796b29c3aaf06
Roman Pinyazhin <pinyazhin@haulmont.com>
2021-09-24 11:10:44 +0400

    Configure per session data source

--- src/main/java/io/jmix/editor/helium/HeliumEditorApplication.java
+++ src/main/java/io/jmix/editor/helium/HeliumEditorApplication.java
@@ -2,15 +2,18 @@ package io.jmix.editor.helium;
 
 import com.google.common.base.Strings;
 import io.jmix.core.security.CoreSecurityConfiguration;
+import io.jmix.editor.helium.sys.HeliumEditorRoutingDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,32 +21,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class HeliumThemeEditorApplication {
+public class HeliumEditorApplication {
 
     @Autowired
     private Environment environment;
 
     public static void main(String[] args) {
-        SpringApplication.run(HeliumThemeEditorApplication.class, args);
+        SpringApplication.run(HeliumEditorApplication.class, args);
     }
 
     @Bean
     @Primary
-    @ConfigurationProperties("main.datasource")
-    DataSourceProperties dataSourceProperties() {
-        return new DataSourceProperties();
+    @ConfigurationProperties(prefix = "routing.datasource")
+    public DataSource dataSource() {
+        return new HeliumEditorRoutingDataSource();
     }
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("main.datasource.hikari")
-    DataSource dataSource(DataSourceProperties dataSourceProperties) {
-        return dataSourceProperties.initializeDataSourceBuilder().build();
+    @Bean("helium_SessionDataSource")
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    @ConfigurationProperties(prefix = "session.datasource")
+    public DataSource sessionDataSource() {
+        return new BasicDataSource();
     }
 
     @EventListener
     public void printApplicationUrl(ApplicationStartedEvent event) {
-        LoggerFactory.getLogger(HeliumThemeEditorApplication.class).info("Application started at "
+        LoggerFactory.getLogger(HeliumEditorApplication.class).info("Application started at "
                 + "http://localhost:"
                 + environment.getProperty("local.server.port")
                 + Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
